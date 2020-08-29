011c7b0f1c0465e0e6db5d5287ab7c5fe5a80856
hussain1998 <E0406406@u.nus.edu>
2020-08-29 19:27:38 +0800

    Update check_syscalls.sh

--- Lab 1/ex5/check_syscalls.sh
+++ Lab 1/ex5/check_syscalls.sh
@@ -2,9 +2,9 @@
 
 ####################
 # Lab 1 Exercise 5
-# Name:
-# Student No:
-# Lab Group: 
+# Name: H Mohamed Hussain
+# Student No: A0199425R
+# Lab Group: 10
 ####################
 
 echo "Printing system call report"
