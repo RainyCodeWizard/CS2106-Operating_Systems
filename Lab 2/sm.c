ac03498499d09534c79003b6d18ea991ecec531d
hussain1998 <E0406406@u.nus.edu>
2020-09-12 13:48:11 +0800

    update

--- Lab 2/sm.c
+++ Lab 2/sm.c
@@ -9,7 +9,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#include <stdio.h>
+#include <stdio.h> //Delete this later
+
+sm_status_t processes[32];
 
 // Use this function to any initialisation if you need to.
 void sm_init(void) {
@@ -21,25 +23,16 @@ void sm_free(void) {
 
 // Exercise 1a/2: start services
 void sm_start(const char *processes[]) {
-    int childPid = fork();
-    //printf("%s",processes[0]);
+    pid_t childPid = fork();
     if(childPid == 0){
         execv(processes[0],processes);
     }
-    //else{
-    //    wait(NULL);
-    //}
-    //
-    //int i = 0;
-    //for (i = 0;i<2;i++){
-    //	printf("%s\n",processes[i]);
-    //}
-    //execv(processes[0], processes);
     return;
 }
 
 // Exercise 1b: print service status
 size_t sm_status(sm_status_t statuses[]) {
+
 }
 
 // Exercise 3: stop service, wait on service, and shutdown
