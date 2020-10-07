df2d82876e4678619ed8d599223b9d5790959de8
H MOHAMED HUSSAIN <bbss1e2hussain@gmail.com>
2020-10-07 15:19:18 +0800

    update

--- Lab 3/ex1-2/test_ex2.c
+++ Lab 3/ex1-2/test_ex2.c
@@ -1,18 +1,18 @@
-#include <"stdio.h">
+#include <stdio.h>
 
 int main(){
     int n;
-    for(n = 1; n<=100; n++){
+    for(n = 1; n<=1000; n++){
         if (n % 3 == 0 && n % 5 == 0){
-            printf("FizzBuzz\n")
+            printf("FizzBuzz\n");
         }
-        if (n % 5 == 0){
-            printf("Buzz\n")
+	else if (n % 5 == 0){
+            printf("Buzz\n");
         }
-        if (n % 3 == 0){
-            printf("Fizz\n")
+	else if (n % 3 == 0){
+            printf("Fizz\n");
         }
-        if (thread ==  1) printf("%d\n",n);
+	else printf("%d\n",n);
     }
     return 0;
 }
