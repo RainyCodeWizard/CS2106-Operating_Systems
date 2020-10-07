ba7317d89aa943e010e95fd0c37f9937d2255139
hussain1998 <E0406406@u.nus.edu>
2020-10-07 12:40:50 +0800

    reusable barrier

--- Lab 3/ex1-2/barrier.c
+++ Lab 3/ex1-2/barrier.c
@@ -13,23 +13,39 @@ void barrier_init ( barrier_t *barrier, int count ) {
     barrier->arrived = 0;
     
     sem_init(&barrier->mutex,0,1);
-    sem_init(&barrier->waitQ,0,0);
+    // sem_init(&barrier->waitQ,0,0);
+    sem_init(&barrier->turnstile1,0,0);
+    sem_init(&barrier->turnstile2,0,1);
 }
 
 void barrier_wait ( barrier_t *barrier ) {
     sem_wait(&barrier->mutex);
     barrier->arrived++;
+    if(barrier->arrived == barrier->count){
+        sem_wait(&barrier->turnstile2);
+        sem_post(&barrier->turnstile1);
+    }
     sem_post(&barrier->mutex);
     
-    if(barrier->arrived == barrier->count){
-        sem_post(&barrier->waitQ);
+    sem_wait(&barrier->turnstile1);
+    sem_post(&barrier->turnstile1);
+
+    sem_wait(&barrier->mutex);
+    barrier->arrived--;
+    if(barrier->arrived == 0){
+        sem_wait(&barrier->turnstile1);
+        sem_post(&barrier->turnstile2);
     }
-    sem_wait(&barrier->waitQ);
-    sem_post(&barrier->waitQ);
+    sem_post(&barrier->mutex);
+
+    sem_wait(&barrier->turnstile2);
+    sem_post(&barrier->turnstile2);
 }
 
 // Perform cleanup here if you need to
 void barrier_destroy ( barrier_t *barrier ) {
     sem_destroy(&barrier->mutex);
-    sem_destroy(&barrier->waitQ);
+    // sem_destroy(&barrier->waitQ);
+    sem_destroy(&barrier->turnstile1);
+    sem_destroy(&barrier->turnstile2);
 }
--- Lab 3/ex1-2/barrier.h
+++ Lab 3/ex1-2/barrier.h
@@ -13,7 +13,9 @@ typedef struct barrier {
   // add additional fields here
   int arrived;
   sem_t mutex; //Binary semaphore to provide mutual exclusion
-  sem_t waitQ; // for N-1 processes to block
+  // sem_t waitQ; // for N-1 processes to block
+  sem_t turnstile1;
+  sem_t turnstile2;
 } barrier_t;
 
 void barrier_init ( barrier_t *barrier, int count );
