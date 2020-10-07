3feb16c8e6d8951997596b1b9ae190839834f1c3
hussain1998 <E0406406@u.nus.edu>
2020-10-07 13:59:19 +0800

    Update fizzbuzz_workers.c

--- Lab 3/ex1-2/fizzbuzz_workers.c
+++ Lab 3/ex1-2/fizzbuzz_workers.c
@@ -8,21 +8,58 @@
                      // implementation
 
 // declare variables to be used here
+#define ensure_successful_malloc(ptr)                           \
+  if (ptr == NULL) {                                            \
+    perror("Memory allocation unsuccessful for" #ptr "\n");     \
+    exit(1);                                                    \
+  }
+
+barrier_t *barrier = malloc( sizeof(barrier_t) );
+ensure_successful_malloc(barrier);
 
 void fizzbuzz_init ( int n ) {
+    barrier_init( barrier, 4 ); // There are only 4 threads
 }
 
 void num_thread( int n, void (*print_num)(int) ) {
+    int i;
+    for(i = 1; i<=n; i++){
+        (*print_num)(i);
+        barrier_wait(barrier);
+    }
 }
 
 void fizz_thread( int n, void (*print_fizz)(void) ) {
+    int i;
+    for(i = 1; i<=n; i++){
+        if(i % 3 == 0){
+            (*print_fizz)();
+        }
+        barrier_wait(barrier);
+    }
 }
 
 void buzz_thread( int n, void (*print_buzz)(void) ) {
+    int i;
+    for(i = 1; i<=n; i++){
+        if(i % 5 == 0){
+            (*print_buzz)();
+        }
+        barrier_wait(barrier);
+    }
 }
 
 void fizzbuzz_thread( int n, void (*print_fizzbuzz)(void) ) {
+    int i;
+    for(i = 1; i<=n; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            (*print_fizzbuzz)();
+        }
+        barrier_wait(barrier);
+    }
 }
 
 void fizzbuzz_destroy() {
+    barrier_destroy(barrier);
+    free(barrier);
 }
