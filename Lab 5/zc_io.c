9e2e1b287f384c0c09370f5271dc5cada1e5e830
hussain1998 <E0406406@u.nus.edu>
2020-11-08 17:59:04 +0800

    Lab 5

---
+++ Lab 5/zc_io.c
@@ -0,0 +1,94 @@
+#include "zc_io.h"
+#include <sys/stat.h>
+
+// The zc_file struct is analogous to the FILE struct that you get from fopen.
+struct zc_file {
+  // Insert the fields you need here.
+  size_t size;
+  void *ptr;
+  int fd;
+  size_t offset;
+};
+
+/**************
+ * Exercise 1 *
+ **************/
+
+zc_file *zc_open(const char *path) {
+  int fd = open(name, O_CREATE | O_RDWR, S_IRWXU|S_IRWXG|S_IRWXO);
+
+  if (fd == -1){
+    return NULL;
+  }
+
+  // To get the size of file
+  struct stat buf;
+  if (fstat(fd, &buf) == -1) return NULL;
+  int size = buf.st_size;
+
+  void *ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  
+  if (ptr == MAP_FAILED) return NULL;
+  
+  zc_file *file = (zc_file *)malloc(sizeof(zc_file));
+  file->size = size;
+  file->ptr = ptr;
+  file->fd = fd;
+  file->offset = 0;
+  
+  return file;
+} 
+
+int zc_close(zc_file *file) {
+  //To implement: Flush to file
+
+  int error = munmap(file->ptr, file->size) | close(file->fd);
+  free(file);
+  return error;
+}
+
+const char *zc_read_start(zc_file *file, size_t *size) {
+  char *ptr = file->ptr;
+  ptr += file->offset;
+  if (file->offset + *size > file->size) {
+    *size = file->size - file->offset;
+  }
+  file->offset += *size;
+  return ptr;
+}
+
+void zc_read_end(zc_file *file) {
+  // To implement
+  // What am i supposed to do here
+}
+
+/**************
+ * Exercise 2 *
+ **************/
+
+char *zc_write_start(zc_file *file, size_t size) {
+  // To implement
+  return NULL;
+}
+
+void zc_write_end(zc_file *file) {
+  // To implement
+}
+
+/**************
+ * Exercise 3 *
+ **************/
+
+off_t zc_lseek(zc_file *file, long offset, int whence) {
+  // To implement
+  return -1;
+}
+
+/**************
+ * Exercise 5 *
+ **************/
+
+int zc_copyfile(const char *source, const char *dest) {
+  // To implement
+  return -1;
+}
